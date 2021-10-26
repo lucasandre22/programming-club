f45a90ce49767bcc76adcf741951b9533bdbc4d1
Lucas <lucas.andre22@yahoo.com>
2021-10-25 22:48:36 -0300

    Add torre_hanoi.cpp

---
+++ Exercises/torre_hanoi.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void solve(int n) {
+    printf("A -> C\n");
+
+
+    printf("%c -> %c\n");
+}
+
+int main(void) {
+
+    return 0;
+}
